8bef0d1887d509fb232bed1f654d1533f164c8af
Isihaaaa <hajdu.istvan05@gmail.com>
2021-02-01 21:55:26 +0100

    Section 3: Node.js Module System (Notes App).

--- Section-3/notes-app/app.js
+++ Section-3/notes-app/app.js
@@ -1,6 +1,20 @@
 // es6 module import fs from 'fs';
 const fs = require("fs");
+const validator = require("validator");
+const chalk = require("chalk");
 
-fs.writeFileSync("note.txt", "Hi my name is Steve.");
+// fs.writeFileSync("note.txt", "Hi my name is Steve.");
 
-fs.appendFileSync("./note.txt", " I'm 27 years old.");
+// fs.appendFileSync("./note.txt", " I'm 27 years old.");
+
+// const add = require("./utiles.js");
+// const getNotes = require("./notes");
+
+// // const name = "Steve";
+// const sum = add(4, -2);
+
+// console.log(getNotes());
+
+// console.log(validator.isEmail("asd@gmail"));
+
+console.log(chalk.red("Success"));
