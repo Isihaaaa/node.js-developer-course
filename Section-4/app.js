ed01973838475a95349eb9ea2523c16e7806a4af
Isihaaaa <hajdu.istvan05@gmail.com>
2021-02-03 19:08:57 +0100

    dont have --help

---
+++ Section-4/app.js
@@ -0,0 +1,30 @@
+// const yargs = require("yargs");
+
+// // const command = process.argv[2];
+
+// // if (command === "add") {
+// //   console.log("Adding note!");
+// // } else if (command === "remove") {
+// //   console.log("Removeing note!");
+// // }
+
+// yargs.version("1.1.0");
+
+// yargs.command("add", true);
+// console.log(process.argv);
+// console.log(yargs.argv);
+// yargs.command({
+//   command: "add",
+//   describe: "Add a new note",
+//   handler: function () {
+//     console.log("Adding a new note!");
+//   },
+// });
+
+// yargs.command({
+//   command: "remove",
+//   describe: "Remove a note",
+//   handler: function () {
+//     console.log("Removeing the note");
+//   },
+// });
